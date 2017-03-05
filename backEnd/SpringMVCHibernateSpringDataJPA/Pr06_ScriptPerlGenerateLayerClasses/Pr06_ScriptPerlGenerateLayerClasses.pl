7b86eb74328601e59b919e5765a1e3e3e5dcb6c2
jeuss7777 <jesuso_arana@yahoo.com>
2017-03-05 14:57:07 -0600

    Update Pr06_ScriptPerlGenerateLayerClasses.pl

--- backEnd/SpringMVCHibernateSpringDataJPA/Pr06_ScriptPerlGenerateLayerClasses/Pr06_ScriptPerlGenerateLayerClasses.pl
+++ backEnd/SpringMVCHibernateSpringDataJPA/Pr06_ScriptPerlGenerateLayerClasses/Pr06_ScriptPerlGenerateLayerClasses.pl
@@ -363,26 +363,7 @@ sub main {
 	print OUTPUT "\t\treturn \"index\";\n";
 	print OUTPUT "\t}\n\n";
 	print OUTPUT "}\n\n";
-					
-#	package com.jarana.controller;
-#
-#	import org.springframework.stereotype.Controller;
-#	import org.springframework.ui.Model;
-#	import org.springframework.web.bind.annotation.RequestMapping;
-#	import org.springframework.web.bind.annotation.ResponseBody;
-#	
-#	@Controller
-#	@RequestMapping("/")
-#	public class HelloController {
-#		@RequestMapping
-#	    @ResponseBody
-#	    public String sayHello(Model model) {
-#	
-#	            model.addAttribute("greeting", "Hello World from Controller");
-#	            return "Welcome_to_my_first_web_services";
-#	    }
-#	
-#	}
+				
 	
 } # main
 
